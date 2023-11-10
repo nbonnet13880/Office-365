c4095c2ffdee020c194367234ed61a9cde68ec23
nbonnet13880 <nbonnet@inyourcloud.fr>
2023-11-10 22:10:57 +0100

    Modify Contact Script

--- Contact/AddContactO365.ps1
+++ Contact/AddContactO365.ps1
@@ -1,27 +1,43 @@
+<#
+.SYNOPSIS 
+This script permit to delete following contacts in Exchange Online. After that importation of new contact can be done.
+
+.DESCRIPTION 
+On the first time, the connection to Exchange Online is performed. We can begin the deletion of contact already in Exchange Online. I use CSV file for delete only the desired contact/
+The contact are imported in the powershell variable then we performed the deletion with the ForEach. The variable $Todelete contain contact that we want delete.
+On the second part, importation has performed. We import a other csv in the powershell variable $Importcsvnew. The path of the csv is configured in line 33 (Variable $Pathcsvnew)
+The ForEach permit to create new mail contact in the M365 tenant.
+
+.LINK 
+https://www.inyourcloud.fr
+https://www.nibonnet.fr
+
+#>
+
 #Connect to Exchange OnLine
 UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 Import-PSSession $Session -DisableNameChecking
 
 #Delete contact on Office 365 if contact is present on CSV file
-$PATHCSV = "C:\DeleteContact.CSV"
-$IMPORTCSV = Import-Csv $PATHCSV
+$Pathcsv = "C:\DeleteContact.CSV"
+$Importcsv = Import-Csv $Pathcsv
 
-ForEach ($item in $IMPORTCSV)
+ForEach ($item in $Importcsv)
 {
-	$TODELETE = $item.ADRESSE
-	Remove-MailContact -Identity $TODELETE -Confirm:$false
-	$TODELETE
+	$Todelete = $item.ADRESSE
+	Remove-MailContact -Identity $Todelete -Confirm:$false
+	$Todelete | Write-Host
 }
 
 #Import contact to CSVFile
 #Create CSV file with column (Display name and External mail) 
-$PATHCSVNEW = "C:\Newcontacts.CSV"
-$IMPORTCSVNEW = Import-Csv $PATHCSVNEW
-ForEach ($itemnew in $IMPORTCSVNEW)
+$Pathcsvnew = "C:\Newcontacts.CSV"
+$Importcsvnew = Import-Csv $Pathcsvnew
+ForEach ($itemnew in $Importcsvnew)
 {
-	$NAME = $itemnew.NAME
-	$ADDRESS = $itemnew.ADDRESS
-	$NAME
-	New-MailContact -Name $NAME -DisplayName $NAME -ExternalEmailAddress $ADDRESS
+	$Name = $itemnew.Name
+	$Address = $itemnew.Address
+	$Name
+	New-MailContact -Name $Name -DisplayName $Name -ExternalEmailAddress $Address
 }
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 You can found script, step by step, etc. All the different resources made available belong to me and I make them available to everyone. 
 
-## Script
+## Contact
 
-* **AddContactO365.ps1** : This script to add new contact on Microsoft Office 365.
+* **AddContactO365.ps1** : This script permit to add new contact on Microsoft Office 365.
 
-This script allows you to connect to Microsoft Exchange. It deletes the contacts present in the csv file. Once the contacts are deleted, the new contacts in the second csv file are imported. This Script allowed me to delete and add several thousand contacts.
+This script allows you to connect to Microsoft Exchange. It deletes the contacts on M365. For that a csv file is used. Once the contacts are deleted, the new contacts in the second csv file are imported. This Script allowed me to delete and add several thousand contacts.
 
 **File required**
 
